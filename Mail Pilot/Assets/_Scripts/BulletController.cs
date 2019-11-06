3a0ff7996bc83cd1947464ee6d28306aa9b7f22d
Zach <zachary_allen@hotmail.com>
2019-11-06 15:24:33 -0500

    Object Pooling implemented

---
+++ Mail Pilot/Assets/_Scripts/BulletController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util;
+
+public class BulletController : MonoBehaviour
+{
+    public float bulletSpeed = 0.1f;
+    public Boundary boundary;
+
+    //TODO: create a reference to the BulletPoolManager
+
+    void Start()
+    {
+        boundary.Top = 2.45f;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CheckBounds();
+    }
+
+    private void Move()
+    {
+        transform.position += new Vector3(0.0f, bulletSpeed, 0.0f);
+    }
+    //Destroys bullet when off screen
+    private void CheckBounds()
+    {
+        if (transform.position.y >= boundary.Top)
+        {
+
+                if (gameObject.tag == "Bullet")
+                {
+                    gameObject.SetActive(false);
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
+            
+
+
+
+            //TODO: This code needs to change to use the BulletPoolManager's
+            //TODO: ResetBullet function which will return the bullet to the pool
+            //    Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Mail Pilot/Assets/_Scripts/BulletPoolManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: Bonus - make this class a Singleton!
+
+[System.Serializable]
+public class BulletPoolManager : MonoBehaviour
+{
+    public static BulletPoolManager SharedInstance;
+
+    void Awake()
+    {
+        SharedInstance = this;
+    }
+
+    //TODO: create a structure to contain a collection of bullets
+    GameObject bullet;
+    public List<GameObject> pooledObjects;
+    public GameObject objectToPool;
+    public int amountToPool;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pooledObjects = new List<GameObject>();
+        for (int i = 0; i < amountToPool; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(objectToPool);
+            obj.SetActive(false);
+            pooledObjects.Add(obj);
+
+        }
+        
+
+        // TODO: add a series of bullets to the Bullet Pool
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //TODO: modify this function to return a bullet from the Pool
+
+    public GameObject GetBullet()
+    {
+        //Iterate through pooled objects
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            //Check to see if bullet is active in scene. If it is, move to next bullet
+            if (!pooledObjects[i].activeInHierarchy)
+            {
+                return pooledObjects[i];
+            }
+        }
+        return null;
+    }
+
+  
+
+
+    //TODO: modify this function to reset/return a bullet back to the Pool 
+    public void ResetBullet(GameObject bullet)
+    {
+
+    }
+}
